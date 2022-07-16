1159077ddaa066794cd9ea9ae4f7c09e104c976d
asbabushkin <asbabushkin@yandex.ru>
2022-07-17 01:03:52 +0500

    web parsing course 5.7.6 exercise added

---
+++ 5.7 test_login_stepic.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+from fake_useragent import UserAgent
+from selenium.webdriver.common.keys import Keys
+from auth_data import stepik_login, stepik_pswd
+
+ua = UserAgent()
+
+options = webdriver.ChromeOptions()
+options.add_argument(f'user-agent={ua.random}')
+
+with webdriver.Chrome(options=options) as step_driver:
+    step_driver.get(url='https://stepik.org/learn?auth=login')
+    sleep(1)
+    login = step_driver.find_element(By.ID, 'id_login_email')
+    sleep(1)
+    password = step_driver.find_element(By.ID, 'id_login_password')
+    sleep(1)
+
+    login.send_keys(stepik_login)
+    sleep(1)
+    password.send_keys(stepik_pswd)
+    sleep(1)
+    password.send_keys(Keys.ENTER)
+    #step_driver.find_element(By.CLASS_NAME, "sign-form__btn button_with-loader ").click()
+    sleep(5)
+    step_driver.get('https://stepik.org/lesson/720527/step/6?unit=721524')
+    sleep(10)
---
+++ 5.7.6 selenium window_size.py
@@ -0,0 +1,23 @@
+"""5.7 Окна и вкладки
+    Откройте сайт с помощью selenium; http://parsinger.ru/window_size/1/
+    Необходимо открыть окно таким размером, чтобы рабочая область страницы составляла 555px на 555px;
+    Учитывайте размеры границ браузера;
+    Результат появится в id="result";
+    Вставьте полученный результат в поле для ответа.
+"""
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+
+
+
+
+with webdriver.Chrome() as browser:
+    browser.get('http://parsinger.ru/window_size/1/')
+    browser.set_window_size(width=555, height=675)
+    print(browser.find_element(By.ID, 'result').text)
+    sleep(3)
+
+
+
+
